437c34dad72dba60f4483473c18122137bd8e0af
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2021-12-10 13:58:18 -0500

    Add --start-maximized flag to webdriver.

--- archivist.py
+++ archivist.py
@@ -411,6 +411,7 @@ def load_webdriver(tmpdir, user=False):
     options = Options()
     options.binary_location = os.environ['CHROME_BIN']
     options.add_argument("--headless")
+    options.add_argument("--start-maximized")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
     prefs = {'download.default_directory' : tmpdir.name}
