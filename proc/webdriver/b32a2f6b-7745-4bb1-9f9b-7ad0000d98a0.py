2c46f364f9e8cb9d2c388eb8e1385a5bf844c78f
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-04-09 15:07:13 -0400

    Remove unused webdriver code for Middlesex-London (ON PHU)

--- proc/webdriver/b32a2f6b-7745-4bb1-9f9b-7ad0000d98a0.py
+++
@@ -1,2 +0,0 @@
-# wait for tab link to be clickable then click
-self.click_xpath(wait, '/html/body/div[1]/report-embed/div/div/div[1]/div/div/div/exploration-container/div/div/div/exploration-host/div/div/exploration/div/explore-canvas/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container[4]/transform/div/div[3]/div/visual-modern')
