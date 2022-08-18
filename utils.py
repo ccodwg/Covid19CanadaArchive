5c21878ffb14dd4081fe3c0f9ee52f0295e69758
Jean-Paul R. Soucy <jeanpaul.r.soucy@gmail.com>
2022-08-17 21:04:55 -0400

    Remove dataset list in README
    - Closes #265
    - Remove generate_readme function from utils.py
    - Update dependencies in utils.py
    - Remove GitHub Actions workflow (.github/workflows/update-readme.yml)
    - Convert to static README (remove docs/README_content.md)
    - Update entry for Data Explorer in README

--- utils.py
+++ utils.py
@@ -5,117 +5,12 @@
 # import modules from Python standard library
 import sys
 import json
-import collections
-
-### gen_readme: Generate README.md (from datasets.json & docs/README_content.md) ###
-def gen_readme():
-  
-  ## function: process metadata into list entries
-  def meta_items(ds, txt):
-    # generic archive index link
-    archive_link = 'https://ccodwg.github.io/Covid19CanadaArchive-data-explorer/'
-    # processing
-    urls = list({d: ds[d]['metadata']['meta_url'] for d in ds}.values())
-    urls = list(dict.fromkeys(urls)) # remove duplicates
-    for url in urls:
-      # get list items
-      items = {k:ds[k] for k in ds.keys() if ds[k]['metadata']['meta_url'] == url}
-      # get url name from first entry
-      url_name = items[list(items)[0]]['metadata']['meta_url_name']
-      # write URL and URL name
-      txt = txt + '* [' + url_name + ']' + '(' + url + ')\n'
-      # write list items
-      for k in items.keys():
-        txt = txt + '    * ' + items[k]['metadata']['meta_name'] + ': ' + \
-        '[' + ds[k]['dir_parent'] + '/' + ds[k]['dir_file'] + '/' + \
-        ds[k]['file_name'] + '.' + ds[k]['file_ext'] + ']' + \
-        '(' + archive_link + ')'
-        # if applicable, add inactive dataset flag
-        if (items[k]['active'] == 'False'):
-          txt = txt + ' [inactive, no longer updated]'
-        # line break
-        txt = txt + '\n'
-    return(txt)
-  
-  ## load datasets.json
-  with open('datasets.json') as json_file:
-          datasets = json.load(json_file)
-  
-  ## note dataset attributes to verify uniqueness
-  uuids = []
-  dirs = []
-  
-  ## flatten datasets into single dictionary
-  ds = {} # create empty dictionary
-  for a in datasets:
-    for b in datasets[a]:
-      for i in range(len(datasets[a][b])):
-        uuids.append(datasets[a][b][i]['uuid'])
-        dirs.append('/'.join([datasets[a][b][i]['dir_parent'], datasets[a][b][i]['dir_file']]))
-        ds[datasets[a][b][i]['uuid']] = datasets[a][b][i]
-  
-  ## verify uniqueness of UUID and directories, otherwise throw error
-  dup_uuids = [item for item, count in collections.Counter(uuids).items() if count > 1]
-  if len(dup_uuids) > 0:
-    sys.exit("There are duplicated UUIDs: " + ", ".join(dup_uuids))
-  else:
-    print("All UUIDs are unique.")
-  dup_dirs = [item for item, count in collections.Counter(dirs).items() if count > 1]
-  if len(dup_dirs) > 0:
-    sys.exit("There are duplicated diectories: " + ", ".join(dup_dirs))
-  else:
-    print("All directories are unique.")
-  
-  ## initialize dataset list
-  print("Generating README...")
-  txt = ""
-  
-  ## get all unique values for top-level groups (meta_group_1) and sort alphabetically
-  g1 = list({ds[d]['metadata']['meta_group_1'] for d in ds})
-  g1.remove('Other: Non-governmental sources')
-  g1.sort()
-  g1.append('Other: Non-governmental sources') # ensure this entry is at the end
-  
-  ## loop through all top-level groups
-  for g in g1:
-    ## header for top-level group
-    txt = txt + '<details>\n<summary><b>' + g + '</b></summary>\n\n'
-    ## subset data
-    dg = {k:ds[k] for k in ds.keys() if ds[k]['metadata']['meta_group_1'] == g}
-    ## check for sub-groups
-    g2 = list({dg[d]['metadata'].get('meta_group_2') for d in dg}) # meta_group_2 only exists if it has a value
-    if None in g2: g2.remove(None) # remove None
-    ## process data without sub-groups
-    dtop = {k:dg[k] for k in dg.keys() if dg[k]['metadata'].get('meta_group_2') is None}
-    txt = meta_items(dtop, txt)
-    ## process data with sub-groups (if they exist)
-    if len(g2) != 0:
-      g2.sort() # alphabetical order
-      for h in g2:
-        txt = txt + '\n<details>\n<summary><i>' + h + '</i></summary>\n\n'
-        dh = {k:dg[k] for k in dg.keys() if dg[k]['metadata'].get('meta_group_2') == h}
-        txt = meta_items(dh, txt)
-        txt = txt + '</details>\n'
-    ## add line break and close <details> tag
-    txt = txt + '</details>\n'
-  
-  ## load README_content.md
-  with open('docs/README_content.md', 'r') as text_file:
-    content = text_file.read()
-  
-  ## add in data catalogue
-  readme = content.replace('[INSERT DATA CATALOGUE HERE]', txt)
-  
-  ## write complete README.md
-  with open('README.md', 'w') as f:
-      f.write(readme)
 
 ### list_inactive_datasets: List datasets that have not been updated in at least 7 days ###
 def list_inactive_datasets():
 
   ## import modules
   import pandas as pd
-  import archivist
   
   ## load active datasets and extract UUIDs as list
   with open('datasets.json') as json_file:
